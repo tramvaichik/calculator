a69560b94a4d4c984639cc3510b21ab0fc4b446f
tramvaichik <arsee@list.ru>
2021-05-17 19:43:50 +0300

    калькулятор №2

---
+++ calc.js
@@ -0,0 +1,64 @@
+function pluse() {
+    var num1, num2, result;
+
+    num1 = document.getElementById('num1').value;
+    num1 = parseInt(num1)
+    console.log(num1);
+
+    num2 = document.getElementById('num2').value;
+    num2 = parseInt(num2);
+    console.log(num2);
+
+    result = num1 + num2;
+    console.log(result);
+
+    document.getElementById('out').innerHTML = result;
+
+}
+
+function subtraction() {
+    var num1, num2, result;
+
+    num1 = document.getElementById('num1').value;
+    num1 = parseInt(num1)
+
+    num2 = document.getElementById('num2').value;
+    num2 = parseInt(num2);
+
+    result = num1 - num2;
+
+    document.getElementById('out').innerHTML = result;
+}
+
+function multiplication() {
+    var num1, num2, result;
+
+    num1 = document.getElementById('num1').value;
+    num1 = parseInt(num1)
+
+
+    num2 = document.getElementById('num2').value;
+    num2 = parseInt(num2);
+
+    result = num1 * num2;
+    console.log(result);
+
+    document.getElementById('out').innerHTML = result;
+}
+
+function division() {
+    var num1, num2, result;
+
+    num1 = document.getElementById('num1').value;
+    num1 = parseInt(num1)
+
+
+    num2 = document.getElementById('num2').value;
+    num2 = parseInt(num2);
+
+
+    result = num1 / num2;
+
+
+    document.getElementById('out').innerHTML = result;
+}
